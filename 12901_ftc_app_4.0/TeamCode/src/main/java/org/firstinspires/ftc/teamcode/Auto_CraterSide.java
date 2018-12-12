b039c0e8a352eb5a548196ce348ca0b4ccfbab27
Randy Pham <randy.default@gmail.com>
2018-12-12 14:11:38 -0700

    added camera skeleton in auto craterside

--- 12901_ftc_app_4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_CraterSide.java
+++ 12901_ftc_app_4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_CraterSide.java
@@ -29,22 +29,31 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import android.graphics.Bitmap;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import for_camera_opmodes.LinearOpModeCamera;
+
 @Autonomous(name = "Auto_CraterSide", group = "Pushbot")
 //@Disabled
-public class Auto_CraterSide extends LinearOpMode {
+public class Auto_CraterSide extends LinearOpModeCamera {
 
     /* Declare OpMode members. */
     Robot robot = new Robot(); // uses the Robot's hardware
     AutoCommands auto = new AutoCommands(); // uses pre-made auto methods
 
+    int ds2 = 2;  // additional downsampling of the image
+    // set to 1 to disable further downsampling
+
     private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() {
+
+        String colorString = "NONE";
 
         /*
          * Initialize the drive system variables.
@@ -56,9 +65,74 @@ public class Auto_CraterSide extends LinearOpMode {
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Status", "Ready to run");    //
         telemetry.update();
+        //Camera auto
+        if (isCameraAvailable()) {
+
+            setCameraDownsampling(8);
+            // parameter determines how downsampled you want your images
+            // 8, 4, 2, or 1.
+            // higher number is more downsampled, so less resolution but faster
+            // 1 is original resolution, which is detailed but slow
+            // must be called before super.init sets up the camera
+
+            telemetry.addLine("Wait for camera to finish initializing!");
+            telemetry.update();
+            startCamera();  // can take a while.
+            // best started before waitForStart
+            telemetry.addLine("Camera ready!");
+            telemetry.update();
+
+            // Wait for driver to press play
+            waitForStart();
+
+            /// Auto code here
+            if (imageReady()) { // only do this if an image has been returned from the camera
+                int redValue = 0;
+                int blueValue = 0;
+                int greenValue = 0;
+
+                // get image, rotated so (0,0) is in the bottom left of the preview window
+                Bitmap rgbImage;
+                rgbImage = convertYuvImageToRgb(yuvImage, width, height, ds2);
+
+                for (int x = 0; x < rgbImage.getWidth(); x++) {
+                    for (int y = 0; y < rgbImage.getHeight(); y++) {
+                        int pixel = rgbImage.getPixel(x, y);
+                        redValue += red(pixel);
+                        blueValue += blue(pixel);
+                        greenValue += green(pixel);
+                    }
+                }
+                int color = highestColor(redValue, greenValue, blueValue);
+
+                switch (color) {
+                    case 0:
+                        colorString = "RED";
+                        break;
+                    case 1:
+                        colorString = "GREEN";
+                        break;
+                    case 2:
+                        colorString = "BLUE";
+                }
+
+                // Proceed with processing image and finding gold cube
+
+            } else {
+                colorString = "NONE";
+
+                // proceed with no camera feed...
+            }
+
+            telemetry.addData("Color:", "Color detected is: " + colorString);
+            telemetry.update();
+            sleep(10);
+        }
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
+
+        //auto code here without camera
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
